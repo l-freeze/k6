2c2df61f446cc13c7803d88cb50bc00f89e56920
l-freeze <mmmm.yamamoto@gmail.com>
2022-09-17 16:25:02 +0900

    request pattern added

--- README.md
+++ README.md
@@ -51,6 +51,40 @@ $ docker-compose run --rm k6 run - < script.js
 $ docker-compose run --rm k6 run --out csv=/output/script_result.csv - < script.js
 ```
 
+結果の見方（予想）
+```
+     ✓ in status 200
+     ✓ authckeck successed
+
+     █ setup
+
+     █ teardown
+
+     checks.........................: 100.00% ✓ 577652      ✗ 0
+     Counting.......................: 288826  9624.867009/s
+     data_received..................: 253 MB  8.4 MB/s
+     data_sent......................: 36 MB   1.2 MB/s
+     http_req_blocked...............: avg=101µs    min=688ns    med=2.79µs  max=215.63ms p(90)=6.06µs  p(95)=9.77µs
+     http_req_connecting............: avg=92.7µs   min=0s       med=0s      max=215.55ms p(90)=0s      p(95)=0s
+     http_req_duration..............: avg=23.96ms  min=87.4µs   med=17.17ms max=1.22s    p(90)=53.3ms  p(95)=71.44ms
+       { expected_response:true }...: avg=23.96ms  min=87.4µs   med=17.17ms max=1.22s    p(90)=53.3ms  p(95)=71.44ms
+     http_req_failed................: 0.00%   ✓ 0           ✗ 288827
+     http_req_receiving.............: avg=181.29µs min=9.79µs   med=34.2µs  max=176.39ms p(90)=84.33µs p(95)=138.38µs
+     http_req_sending...............: avg=183.34µs min=4.27µs   med=12.52µs max=749.56ms p(90)=32.97µs p(95)=50.95µs
+     http_req_tls_handshaking.......: avg=0s       min=0s       med=0s      max=0s       p(90)=0s      p(95)=0s
+     http_req_waiting...............: avg=23.6ms   min=49.93µs  med=16.93ms max=1.22s    p(90)=52.56ms p(95)=70.47ms
+     http_reqs......................: 288827  9624.900333/s
+     iteration_duration.............: avg=42.55ms  min=177.17µs med=28.03ms max=1.31s    p(90)=98.21ms p(95)=138.49ms
+     iterations.....................: 288826  9624.867009/s
+     vus............................: 25      min=0         max=988
+     vus_max........................: 1099    min=1099      max=109
+
+http_reqs...トータルリクエスト数（アクセス数/秒）
+vus_max...瞬間最大風速（同時アクセス数）
+iterations...シナリオ実行回数
+http_req_failed...文字通りリクエスト失敗の割合か？localだと失敗しないので不明
+```
+
 # grafana
 ログイン
 ```
--- script_local.js
+++ script_local.js
@@ -1,5 +1,7 @@
+//docker-compose run --rm k6 run -o xk6-influxdb=http://influxdb:8086/k6db - < script_local.js
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { Counter } from 'k6/metrics';
 
 const FQDN = 'k6-web';
 const URLs = {
@@ -9,13 +11,110 @@ const URLs = {
     '_50x': `http://${FQDN}/50x.html`,
 };
 
+//https://k6.io/docs/getting-started/running-k6/#stages-ramping-up-down-vus
+//高度なoptionはシナリオ使え、との事
+//シナリオ：https://k6.io/docs/using-k6/scenarios#configuration
+/*
 export const options = {
     stages: [
+        //--------
+        // duration: 試行時間（負荷をかける時間）
+        // target: 同時接続数 (cliオプションの --vus)
+        //--------
         { duration: '3s', target: 2},
         //{ duration: '1m5s', target: 10},
         //{ duration: '2s', target: 0},
     ]
 };
+*/
+
+//総アクセス数で負荷掛けるのが普通だろうからexecutorはshared-iterationsが良さそうだ
+export const options = {
+    scenarios: {
+        //100user * 10access
+        scenario_01: {
+            // name of the executor to use
+            //executor: 'shared-iterations',
+            executor: 'per-vu-iterations',//1vuに対してiteration回試行させる
+            // https://k6.io/docs/using-k6/scenarios/executors/per-vu-iterations/
+            
+            exec: 'access20x',
+
+            // common scenario configuration
+            startTime: '0s',
+            gracefulStop: '15s',
+            env: { EXAMPLEVAR: 'testing' },
+            tags: { 
+                'author': 'l-freeze',
+            },
+
+            // executor-specific configuration
+            vus: 100,
+            iterations: 10,
+            maxDuration: '10s',
+        },
+
+    /*
+        // 2000access/2000user (1user 1access)
+        scenario_11: {
+            //executor: 'per-vu-iterations',
+            executor: 'shared-iterations',//2000vuで合計iteration回試行できればOK。1vuが何回リクエストするかは問わない
+            //https://k6.io/docs/using-k6/scenarios/executors/shared-iterations/
+
+            exec: 'access40x',
+
+            startTime: '5s',
+            gracefulStop: '15s',
+            env: { EXAMPLEVAR: 'testing' },
+            tags: { 
+                'author': 'l-freeze',
+            },  
+
+            vus: 2000,
+            iterations: 2000,
+            maxDuration: '10s',
+        },
+
+        //40000access/100user (100user 40access)
+        scenario_21: {
+            //executor: 'per-vu-iterations',
+            executor: 'shared-iterations',
+            exec: 'access50x',
+
+            startTime: '10s',
+            gracefulStop: '15s',
+            env: { EXAMPLEVAR: 'testing' },
+            tags: { 
+                'author': 'l-freeze',
+            },  
+
+            vus: 100,
+            iterations: 40000,
+            maxDuration: '10s',
+            },
+    */
+
+
+        //オートスケールのテストするならramping-vusが良いかもしれん
+        scenario_31: {
+            startTime: '5s',
+            executor: 'ramping-vus',
+            exec: 'access20x',
+            stages: [
+                { duration: '20s', target: 1000 },//20秒掛けて100VUに
+                { duration: '5s', target: 0 },//5秒掛けて0VUに
+            ],
+            gracefulRampDown: '10s',//0にするとリクエスト中でもぶった切られるので
+            gracefulStop: '15s',
+            env: { EXAMPLEVAR: 'testing' },
+            tags: { 
+                'author': 'l-freeze',
+            },
+        },
+    },
+};
+  
+const counter = new Counter('Counting');
 
 export function setup() {
     console.log(URLs.Auth);
@@ -35,6 +134,7 @@ export function setup() {
 }
 
 export default function (setUpData) {
+    counter.add(1);
     const payload = JSON.stringify({
         'email': 'l-freeze@example.com',
         'password': 'password',
@@ -48,10 +148,63 @@ export default function (setUpData) {
     const res = http.get(URLs._20x, params);
     //const res = http.post(URLs._20x, payload, params);
 
-    check(res, { 'status was 200': (r) => r.status == 200 });
-    check(res, { 'status was 200': (r) => r.headers['Auth-Check'] == 'Successed' });
+    check(res, { 
+        'in status 200': (r) => r.status === 200 ,
+        'authckeck successed': (r) => r.headers['Auth-Check'] === 'Successed' ,
+        'authckeck failed': (r) => r.headers['Auth-Check'] === 'Failed' ,
+    });
+    //check(res, { 'status was 200': (r) => r.status == 200 });
+    //check(res, { 'authckeck is ': (r) => r.headers['Auth-Check'] });
+    //check(res, { 'authckeck result ': (r) => r.headers['Auth-Check'] == 'Successed' });
     //check(res, { 'status was 200': (r) => r.headers['Auth-Check'] == 'Failed' });
 }
+
+export function access20x(setUpData) {
+    counter.add(1);
+    const params = {
+        'headers': {
+            'Content-Type': 'application/json',
+            'X-token': setUpData.token,
+        },
+    }
+    const res = http.get(URLs._20x, params);
+
+    check(res, { 
+        'in status 200': (r) => r.status === 200 ,
+        'authckeck successed': (r) => r.headers['Auth-Check'] === 'Successed' ,
+    });
+}
+
+export function access40x(setUpData) {
+    counter.add(1);
+    const params = {
+        'headers': {
+            'Content-Type': 'application/json',
+            'X-token': setUpData.token,
+        },
+    }
+    const res = http.get(URLs._40x, params);
+
+    check(res, { 
+        'in status 404': (r) => r.status === 404 ,
+    });
+}
+
+export function access50x(setUpData) {
+    counter.add(1);
+    const params = {
+        'headers': {
+            'Content-Type': 'application/json',
+            'X-token': setUpData.token,
+        },
+    }
+    const res = http.get(URLs._50x, params);
+
+    check(res, { 
+        'in status 500': (r) => r.status === 500 ,
+    });
+}
+
 
 export function teardown(setUpData) {
     console.log(setUpData);
